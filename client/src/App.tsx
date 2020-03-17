7c4f9cae5afa9044d96a9e278a81fab3eb519c5c
Cheryl Hughey <cherylhughey@hotmail.com>
2020-03-17 14:25:54 -0500

    wireframe changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,17 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import User from './components/User';
 import { UserInfo } from './interfaces';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import FormControl from 'react-bootstrap/FormControl';
+import Form from 'react-bootstrap/FormCheck';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Image from 'react-bootstrap/Image';
+import Tooltip from 'react-bootstrap/Tooltip';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import './App.scss';
 import TestForm from './components/Form';
 
@@ -37,13 +48,50 @@ function App() {
 
     return (
         <div className="app">
-            <header className="app-header">
-                CIB-Management
-            </header>
-            <div className="main">
-                <User {...user} />
-                <TestForm />
-            </div>
+            <Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#home">CIB-Management</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#link">Link</Nav.Link>
+      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-success">Search</Button>
+    </Form>
+  </Navbar.Collapse>
+</Navbar>
+
+        <div className="main">
+          <Image src="https://via.placeholder.com/2560X250" fluid />            
+            <Jumbotron>
+              <User {...user} />
+              <TestForm />
+            </Jumbotron>
+        </div>
+
+        <div className="footer">
+            <Navbar>
+              <Navbar.Text>
+                Copyright disclaimers, etc. to point somewhere
+              </Navbar.Text>
+            
+              <Navbar.Collapse className="justify-content-end">
+                <Navbar.Text>
+                  Signed in as: <a href="#login">Jane Doe</a>
+                </Navbar.Text>
+              </Navbar.Collapse>
+            </Navbar>
+        </div>
+
         </div>
     );
 }
