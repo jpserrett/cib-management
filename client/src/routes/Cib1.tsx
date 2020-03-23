c47c1336a0766eea461c1675b1e86ed4b9e26a61
Philip <jpserrett@gmail.com>
2020-03-23 14:30:47 -0500

    sample CIB1 form created

--- client/src/routes/Cib1.tsx
+++ client/src/routes/Cib1.tsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
+import { TextInput, Select } from '../components/commonFormComponents';
 import { Cib1Type } from '../interfaces';
 
 const Cib1 = () => {
@@ -36,6 +39,56 @@ const Cib1 = () => {
         <>
             <div>CIB1s</div>
             <span>{`NSN: ${cib1.nsn}`}</span>
+            <Formik
+              initialValues={cib1}
+              enableReinitialize
+              validationSchema={Yup.object({
+                  producer: Yup.string()
+                      .max(15, 'Must be 15 characters or less')
+                      .required('Required'),
+                  edition: Yup.number()
+                      .max(1, 'May only be a single digit')
+                      .required('Required'),
+              })}
+              onSubmit={(values, { setSubmitting }) => {
+                  setTimeout(() => {
+                      console.warn(JSON.stringify(values, null, 2));
+                      setSubmitting(false);
+                  }, 400);
+              }}
+            >
+                <Form>
+                    <TextInput
+                      id="edition"
+                      label="Edition"
+                      name="edition"
+                      type="text"
+                    />
+                    <TextInput
+                      id="nsn"
+                      label="NSN"
+                      name="nsn"
+                      type="text"
+                    />
+                    <TextInput
+                      id="prodStatus"
+                      label="Production Status"
+                      name="prodStatus"
+                      type="text"
+                    />
+                    <Select
+                      id="producer"
+                      label="Producer"
+                      name="producer"
+                      type="text"
+                    >
+                        <option>Harris</option>
+                        <option>BAE</option>
+                        <option>ACME</option>
+                    </Select>
+                    <button type="submit">Submit</button>
+                </Form>
+            </Formik>
         </>
     );
 };
